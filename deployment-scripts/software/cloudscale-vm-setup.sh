b54530dd334c2706fc87c4fef07dc59912d847ff
Simon Ivanšek <simon.ivansek@xlab.si>
2014-03-12 11:09:53 +0100

    Remove large file

--- deployment-scripts/software/cloudscale-vm-setup.sh
+++ deployment-scripts/software/cloudscale-vm-setup.sh
@@ -2,13 +2,8 @@
 
 sudo apt-get install -y zip tomcat7 apache2 openjdk-7-jdk
 
-sudo a2enmod proxy proxy_http
+sudo a2enmod proxy
 sudo cp cloudscale.conf /etc/apache2/sites-available/
 sudo a2ensite cloudscale
 sudo /etc/init.d/apache2 restart
-
-sudo ln -s /var/lib/tomcat7/shared /usr/share/tomcat7/shared
-sudo ln -s /var/lib/tomcat7/server /usr/share/tomcat7/server
-sudo ln -s /var/lib/tomcat7/common /usr/share/tomcat7/common
-
 sudo /etc/init.d/tomcat7 restart
