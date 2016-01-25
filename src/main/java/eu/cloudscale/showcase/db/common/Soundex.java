3306a2c6cba16863ecc05ddcca6860c678dd1cb5
Simon Ivanšek <simon.ivansek@xlab.si>
2016-01-25 10:47:55 +0100

    Added license header to all files

--- src/main/java/eu/cloudscale/showcase/db/common/Soundex.java
+++ src/main/java/eu/cloudscale/showcase/db/common/Soundex.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+*  Copyright (c) 2015 XLAB d.o.o.
+*  All rights reserved. This program and the accompanying materials
+*  are made available under the terms of the Eclipse Public License v1.0
+*  which accompanies this distribution, and is available at
+*  http://www.eclipse.org/legal/epl-v10.html
+*******************************************************************************/
 package eu.cloudscale.showcase.db.common;
 
 public class Soundex
