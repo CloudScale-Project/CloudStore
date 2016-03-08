d2d9aba81b2ed678e9e17549c06ca0d1b3b01b5e
Simon Ivanšek <simon.ivansek@xlab.si>
2016-03-08 13:58:13 +0100

    Add author in header of each file

--- src/main/java/eu/cloudscale/showcase/db/model/mongo/ShoppingCartLine.java
+++ src/main/java/eu/cloudscale/showcase/db/model/mongo/ShoppingCartLine.java
@@ -4,6 +4,8 @@
 *  are made available under the terms of the Eclipse Public License v1.0
 *  which accompanies this distribution, and is available at
 *  http://www.eclipse.org/legal/epl-v10.html
+*  
+*  @author XLAB d.o.o.
 *******************************************************************************/
 package eu.cloudscale.showcase.db.model.mongo;
 
